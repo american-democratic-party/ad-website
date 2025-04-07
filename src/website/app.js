b2f85db79bd24fc580fd0bf7338f4b24230e54c1
dpilafian <dem@centerkey.com>
2025-04-06 17:42:34 -0700

    Placeholder website

---
+++ src/website/app.js
@@ -0,0 +1,50 @@
+// American Democratic Party
+
+const app = {
+
+   article: {
+      setupPage() {
+         // <header>
+         //    <div id=article-nav>
+         //       <i data-icon=circle-left>
+         //       <i data-icon=circle-right>
+         //       <ul>
+         //          <li><a>
+         // <main>
+         //    <section>
+         //       <footer>
+         const container = globalThis.document.getElementById('article-nav');
+         const nav =       { prev: container.children[0], next: container.children[1] };
+         const articles =  [...container.querySelectorAll('ul >li >a')];
+         const header =    'main >section:first-child >h2';
+         const title =     globalThis.document.querySelector(header).textContent;
+         const index =     articles.findIndex(article => article.textContent === title);
+         const configure = (button, index, leadingText) => {
+            button.setAttribute('data-href', articles[index]?.getAttribute('href'));
+            button.setAttribute('title',     `${leadingText} "${articles[index]?.textContent}"`);
+            button.classList.add(index > -1 && index < articles.length ? 'show' : 'hide');
+            };
+         configure(nav.prev, index - 1, 'View');
+         configure(nav.next, index + 1, 'Press ENTER to view');
+         container.classList.add('show');
+         const iconBar = globalThis.document.querySelector('section >footer');
+         iconBar.setAttribute('title', 'Press ENTER to view the next article.');
+         const jumpToNextArticle = () => nav.next.classList.contains('show') && nav.next.click();
+         dna.dom.onEnterKey(jumpToNextArticle);
+         dna.dom.onKeyUp((elem, event) => event.key === '1' && articles[0].click());
+         },
+      init() {
+         const hasArticleTitle = /\/article\/./;
+         if (hasArticleTitle.test(globalThis.location.href))
+            app.article.setupPage();
+         },
+      },
+
+   start() {
+      console.log('🇺🇸 American Democratic Party 🇺🇸');
+      app.article.init();
+      },
+
+   };
+
+dna.dom.onReady(app.start);
