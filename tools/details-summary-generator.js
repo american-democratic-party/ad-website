ff95e9de83c6f120f88324bc7e1e8dbab146592d
dpilafian <dem@centerkey.com>
2025-04-22 15:25:54 -0700

    Automatically generate platform toc

--- tools/details-summary-generator.js
+++ tools/details-summary-generator.js
@@ -11,40 +11,58 @@ import slash from 'slash';
 //    <p>These are some details.</p>
 //    <p>These are some details.</p>
 // </details>
+//
+// Usage:
+//    node details-summary-generator.js [source] [destination] [name]
 
-const sourceFolder = path.normalize(slash(process.argv[2]));
-const outputFile =   path.normalize(slash(process.argv[3]));
+const cleanPath =    (folder) => path.normalize(slash(folder)).trim().replace(/\/$/, '');
+const sourceFolder = cleanPath(process.argv[2]);
+const outputFolder = cleanPath(process.argv[3]);
+const outputName =   process.argv[4].trim().toLowerCase();
 const files =        globSync(sourceFolder + '/**/*.html').map(slash).sort();
+const tocFile =      `${outputFolder}/${outputName}-toc.html`;
+const detailsFile =  `${outputFolder}/${outputName}-details.html`;
+const toc =          [];
 
 const intro = () => {
    console.log('[Details Summary Generator Tool]');
    console.log('source:   ', sourceFolder);
-   console.log('output:   ', outputFile);
+   console.log('toc:      ', tocFile);
+   console.log('details:  ', detailsFile);
    console.log('templates:', files.length);
    };
 
 const processFile = (filePath, index) => {
+   const html =         fs.readFileSync(filePath, 'utf-8');
    const pathInfo =     path.parse(filePath);
    const isParentNode = pathInfo.base === '+summary-header.html';
    const isLast =       index === files.length - 1;
-   const sectionCode =  isParentNode ? path.basename(pathInfo.dir) : pathInfo.name;  //ex: "2-tax-policy"
-   const id =           sectionCode.slice(sectionCode.indexOf('-') + 1);  //ex: "tax-policy"
    const sourceDepth =  sourceFolder.split('/').length;
    const fileDepth =    filePath.split('/').length;
    const calcPops =     (file) => Math.max(0, fileDepth - file.split('/').length);  //levels to unnest
    const pops =         isLast ? fileDepth - sourceDepth - 1 : calcPops(files[index + 1]);
-   const sectionBase =  (isParentNode ? pathInfo.dir : filePath).slice(sourceFolder.length + 1);  //ex: "3-domestic/2-tax-policy"
-   const sectonNumber = sectionBase.split('/').map(section => section.split('-')[0]).join('.');  //ex: "3.2"
-   const html = fs.readFileSync(filePath, 'utf-8')
-      .replace('<details>', `<details id=${id}>`)
-      .replace('<summary>', `<summary>${sectonNumber}: `)
+   const sectionCode =  isParentNode ? path.basename(pathInfo.dir) : pathInfo.name;  //ex: "5-tax-policy"
+   const sectionBase =  (isParentNode ? pathInfo.dir : filePath).slice(sourceFolder.length + 1);  //ex: "3-domestic/5-tax-policy"
+   const section = {
+      id:    sectionCode.slice(sectionCode.indexOf('-') + 1),                         //ex: "tax-policy"
+      depth: fileDepth - sourceDepth - (isParentNode ? 1 : 0),                        //ex: 2
+      title: html.split('summary>')[1].slice(0, -2),                                  //ex: "Tax Policy"
+      num:   sectionBase.split('/').map(section => section.split('-')[0]).join('.'),  //ex: "3.5"
+      };
+   toc.push(section);
+   const details = html
+      .replace('<details>', `<details id=${section.id}>`)
+      .replace('<summary>', `<summary>${section.num}: `)
       .replace('</details>', isParentNode ? '' : '</details>') +
       '</details>\n'.repeat(pops);
-   return html;
+   return details;
    };
 
 const processFiles = () => {
-   fs.writeFileSync(outputFile, files.map(processFile).join('\n'));
+   fs.writeFileSync(detailsFile, files.map(processFile).join('\n'));
+   const tocLine = (section) =>
+      `<li data-depth=${section.depth}><a href=#${section.id}>${section.num}: ${section.title}</a></li>`;
+   fs.writeFileSync(tocFile, toc.map(tocLine).join('\n') + '\n');
    };
 
 intro();
